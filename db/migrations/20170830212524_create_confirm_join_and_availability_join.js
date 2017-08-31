cf9bd891a752b1569992a95375c7763f9859c532
Jay Lim <jhjaylim@gmail.com>
2017-08-30 21:39:52 -0700

    database schema created

---
+++ db/migrations/20170830212524_create_confirm_join_and_availability_join.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('confirmed', function (table) {
+      table.increments('id').unsigned().primary();
+      table.integer('user_id').references('id').inTable('users');
+      table.integer('trip_id').references('id').inTable('trips');
+    }),
+    knex.schema.createTableIfNotExists('availability', function(table) {
+      table.increments('id').unsigned().primary();
+      table.integer('user_id').references('id').inTable('users');
+      table.integer('trip_id').references('id').inTable('trips');
+      table.string('start').notNullable();
+      table.string('end').notNullable();
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('confirmed'),
+    knex.schema.dropTable('availability')
+  ]);
+};
