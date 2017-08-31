cacebf6ccbea81bde1aa5c05c0147e33a3c07048
jsonlitz <jasondotlee@gmail.com>
2017-08-31 12:01:08 -0700

    add Event.jsx

--- client/src/app.js
+++ client/src/app.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dashboard from './components/DashBoard.jsx'
+import Dashboard from './components/DashBoard.jsx';
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
 
-    }
+    };
   }
 
-render(){
-  return (
-    <div>
-      <h1>Hello World from React</h1>
-      <Dashboard />
-    </div>
-  )
-}
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
