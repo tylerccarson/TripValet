d84574f1a229fc6a2c2eb6400e3a1e92aa57c698
jsonlitz <jasondotlee@gmail.com>
2017-09-02 12:04:58 -0700

    Fix indentation and spacing in DashBoard.jsx

--- client/src/components/DashBoard.jsx
+++ client/src/components/DashBoard.jsx
@@ -4,11 +4,11 @@ import { Jumbotron, Button } from 'react-bootstrap';
 const DashBoard = (props) => (
   <div>
     <Jumbotron>
-    <h1>Upcoming Trips</h1>
-    <p>Trip one</p>
-    <p>Trip two</p>
+      <h1>Upcoming Trips</h1>
+      <p>Trip one</p>
+      <p>Trip two</p>
     
-    <p><Button bsStyle="primary">Create</Button></p>
+      <p><Button bsStyle="primary">Create</Button></p>
     </Jumbotron>
   </div>
 );
