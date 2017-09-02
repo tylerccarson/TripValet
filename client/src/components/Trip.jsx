a43d1d391327b987992f79d80e5f0352e9b8721e
Jason Lee <jasondotlee@gmail.com>
2017-09-01 19:24:44 -0700

    Update Trip.jsx

--- client/src/components/Trip.jsx
+++ client/src/components/Trip.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
-const Event = (props) => (
+const Trip = (props) => (
   <div>
-<<<<<<< 57e394c6eb0380a5266a62306ff12826399497b7:client/src/components/Trip.jsx
-    <p>This is the Trip</p>
-=======
     <h1>This is the Event</h1>
->>>>>>> Add Header.jsx and Main.jsx, add React Router, reformat into react-router:client/src/components/Event.jsx
   </div>
 );
 
