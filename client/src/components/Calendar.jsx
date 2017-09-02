5d9a0a9aa0279c73865d23cb67ae1e4837933200
weiyilee17 <weiyilee17@gmail.com>
2017-09-02 12:08:02 -0700

    had simple click event work

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -8,82 +8,41 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 // to the correct localizer.
 BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
+// TODO: click again to unpick
+// be able to set range for long dates
+// only put your name once per day
+
+
 class Calendar extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      eventList: [
-        {
-          'title': 'All Day Event',
-          'allDay': true,
-          'start': new Date(2015, 3, 0),
-          'end': new Date(2015, 3, 1)
-        },
+      userName: 'Lee',  // hard coded for now
+      eventList: [  // hard coded for now
         {
           'title': 'Long Event',
-          'start': new Date(2015, 3, 7),
-          'end': new Date(2015, 3, 10)
-        },
-      
-        {
-          'title': 'DTS STARTS',
-          'start': new Date(2016, 2, 13, 0, 0, 0),
-          'end': new Date(2016, 2, 20, 0, 0, 0)
-        },
-      
-        {
-          'title': 'DTS ENDS',
-          'start': new Date(2016, 10, 6, 0, 0, 0),
-          'end': new Date(2016, 10, 13, 0, 0, 0)
-        },
-      
-        {
-          'title': 'Some Event',
-          'start': new Date(2015, 3, 9, 0, 0, 0),
-          'end': new Date(2015, 3, 9, 0, 0, 0)
-        },
-        {
-          'title': 'Conference',
-          'start': new Date(2015, 3, 11),
-          'end': new Date(2015, 3, 13),
-          desc: 'Big conference for important people'
-        },
-        {
-          'title': 'Meeting',
-          'start': new Date(2015, 3, 12, 10, 30, 0, 0),
-          'end': new Date(2015, 3, 12, 12, 30, 0, 0),
-          desc: 'Pre-meeting meeting, to prepare for the meeting'
-        },
-        {
-          'title': 'Lunch',
-          'start':new Date(2015, 3, 12, 12, 0, 0, 0),
-          'end': new Date(2015, 3, 12, 13, 0, 0, 0),
-          desc: 'Power lunch'
-        },
-        {
-          'title': 'Meeting',
-          'start':new Date(2015, 3, 12,14, 0, 0, 0),
-          'end': new Date(2015, 3, 12,15, 0, 0, 0)
-        },
-        {
-          'title': 'Happy Hour',
-          'start':new Date(2015, 3, 12, 17, 0, 0, 0),
-          'end': new Date(2015, 3, 12, 17, 30, 0, 0),
-          desc: 'Most important meal of the day'
-        },
-        {
-          'title': 'Dinner',
-          'start':new Date(2015, 3, 12, 20, 0, 0, 0),
-          'end': new Date(2015, 3, 12, 21, 0, 0, 0)
-        },
-        {
-          'title': 'Birthday Party',
-          'start':new Date(2015, 3, 13, 7, 0, 0),
-          'end': new Date(2015, 3, 13, 10, 30, 0)
+          'start': new Date(2017, 8, 7),
+          'end': new Date(2017, 8, 10)
         }
       ]
     }
+
+    this.pickDate = this.pickDate.bind(this);
+  }
+
+  pickDate(pickedSlot) {
+    // create duplicate cause Oleg said we shouldn't be altering state directly
+    var eventListDuplicate = this.state.eventList.slice();
+
+    eventListDuplicate.push({
+      'title' : this.state.userName,
+      'start' : pickedSlot.start,
+      'end' : pickedSlot.end
+    });
+    this.setState({
+      eventList: eventListDuplicate
+    });
   }
 
   render() {
@@ -99,8 +58,21 @@ class Calendar extends React.Component {
     return (
       <div style={style} {...this.props}>
         <BigCalendar
+          selectable
           events = {this.state.eventList}
-          defaultDate={new Date(2015, 3)}
+          defaultDate={new Date()}
+          onSelectEvent={ (event) => 
+            {
+              alert(event.title);
+            }
+          }
+          onSelectSlot={ (slotInfo) => 
+            {
+              console.log('slotInfo: ', slotInfo);
+              this.pickDate(slotInfo);
+            }
+            
+          }
         />
       </div>
     );
