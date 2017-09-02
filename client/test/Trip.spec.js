ce4c315b1137930907971745743a7d6dfa76f3e5
tyler carson <tyler.c.carson@gmail.com>
2017-09-01 17:28:02 -0700

    chang event to trip

--- client/test/Trip.spec.js
+++ client/test/Trip.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Event from '../src/components/Trip.jsx';
+import Trip from '../src/components/Trip.jsx';
 
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
