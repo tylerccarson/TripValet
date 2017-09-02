705464b0b065fee6253987eb9c98f3b5345b3b1e
jsonlitz <jasondotlee@gmail.com>
2017-09-02 12:25:04 -0700

    add dependencies for dashboard.spec.js

--- client/test/Dashboard.spec.js
+++ client/test/Dashboard.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DashBoard from '../src/components/DashBoard.jsx';
+import { Jumbotron, Button } from 'react-bootstrap';
 
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
