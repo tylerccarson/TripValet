02e8d2b814574a53ace59f583bf6c278b14b2768
Jay Lim <jhjaylim@gmail.com>
2017-09-01 21:10:52 -0700

    fixed module reference

--- db/models/index.js
+++ db/models/index.js
@@ -1,6 +1,6 @@
 module.exports.Auth = require('./auths');
 module.exports.Profile = require('./profiles');
-module.exports.Trip = require('./Trip');
-module.exports.User = require('./User');
+module.exports.Trip = require('./trip');
+module.exports.User = require('./user');
 module.exports.Availability = require('./Availability');
 module.exports.Confirmed = require('./Confirmed');
