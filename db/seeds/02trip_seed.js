a5337949009e53ec796f161651c26841c709e9d0
Jay Lim <jhjaylim@gmail.com>
2017-09-07 16:47:09 -0700

    date ranges format changed

--- db/seeds/02trip_seed.js
+++ db/seeds/02trip_seed.js
@@ -2,12 +2,20 @@ const models = require('../models');
 
 exports.seed = function (knex, Promise) {
 
-  return models.Trip.where({ id: '1' }).save(
-    {
-      rangeStart: '2017/10/01',
-      rangeEnd: '2017/10/30'
-    }
-  )
+  return models.Trip.where({ id: '1' }).fetch()
+    .then((trip) => {
+      if (trip) {
+        throw trip;
+      }
+      return models.Trip.forge({
+        tripname: `Adam's bachelors party`,
+        description: 'You know the drill. Anything happens in LV, stays in LV',
+        location: 'Las Vegas',
+        rangeStart: '20171001',
+        rangeEnd: '20171030',
+        user_id: 1
+      }).save();
+    })
     .error((err)=>{
       console.error('Failed to create new trip');
       throw err;
