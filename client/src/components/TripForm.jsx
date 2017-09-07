b03c31f289b602035c34a1dada1d785d6db712b2
jsonlitz <jasondotlee@gmail.com>
2017-09-07 14:52:49 -0700

    started form form

---
+++ client/src/components/TripForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FormGroup, InputGroup, FormControl, Button, DropdownButton, MenuItem, Grid, HelpBlock } from 'react-bootstrap';
+
+
+class TripForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  };
+
+  render () {
+    return (
+      <form>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Item Name"
+              name="name"
+              value={this.state.name}
+              onChange={this.onChange}
+              />
+            <InputGroup.Addon></InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+      </form>
+    )
+  }
+}
+
+export default TripForm;
