80173467122f1593b06e5edbcc737861f150b820
jsonlitz <jasondotlee@gmail.com>
2017-09-07 15:34:32 -0700

    add esc out of modal functionality

--- client/src/components/DashBoard.jsx
+++ client/src/components/DashBoard.jsx
@@ -15,6 +15,7 @@ class DashBoard extends React.Component {
     console.log('Dashboard Constructor says hello');
     this.hideModal = this.hideModal.bind(this);
     this.showModal = this.showModal.bind(this);
+    this.close = this.close.bind(this);
   }
   hideModal(e) {
     this.setState({
@@ -28,11 +29,20 @@ class DashBoard extends React.Component {
     });
   }
 
+  close() {
+    this.setState({
+      lgShow:false
+    });
+  }
 
   render() {
     return (
       <div>
-        <Modal show={this.state.lgShow} bsSize="large" aria-labelledby="contained-modal-title-sm">
+        <Modal
+          show={this.state.lgShow}
+          onHide={this.close}
+          bsSize="large"
+          aria-labelledby="contained-modal-title-sm">
           <Modal.Header >
             <Modal.Title id="contained-modal-title-sm">Create New Trip</Modal.Title>
           </Modal.Header>
