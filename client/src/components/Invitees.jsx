7ae4d751d9bc170702ac89e49525cbec51a9b60c
jsonlitz <jasondotlee@gmail.com>
2017-09-08 11:56:17 -0700

    add invitee component, render current invite list to form

---
+++ client/src/components/Invitees.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Invitees = (props) => (
+  <div>
+    <h3>Invitees</h3>
+    <ul>
+      <li>{props.invitee}</li>
+    </ul>
+  </div>
+);
+export default Invitees;
