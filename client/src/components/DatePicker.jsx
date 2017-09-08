0748c6c690ac4adad8f5c69224063afe9aa152e9
jsonlitz <jasondotlee@gmail.com>
2017-09-07 20:23:14 -0700

    add date picker to form

---
+++ client/src/components/DatePicker.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DatePicker from 'material-ui/DatePicker';
+
+const DatePickers = () => (
+  <div>
+    <DatePicker mode="landscape" />
+  </div>
+);
+
+export default DatePickers;
