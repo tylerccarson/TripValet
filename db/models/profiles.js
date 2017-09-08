6140306b3c03af0dac9e6ed8ae31dcbadf506191
Jay Lim <jhjaylim@gmail.com>
2017-09-07 21:53:07 -0700

    migrated user foreign key in users table to profiles table

--- db/models/profiles.js
+++ db/models/profiles.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const db = require('../');
 
 const Profile = db.Model.extend({
@@ -20,3 +21,26 @@ const Profile = db.Model.extend({
 });
 
 module.exports = db.model('Profile', Profile);
+=======
+const db = require('../');
+
+const Profile = db.Model.extend({
+  tableName: 'profiles',
+  auths: function() {
+    return this.hasMany('Auth');
+  },
+  trips: function() {
+    return this.hasMany('Trip');
+  },
+  confirmed: function() {
+    return this.hasMany('Confirmed');
+  },
+  availability: function() {
+    return this.hasMany('Availability');
+  }
+
+
+});
+
+module.exports = db.model('Profile', Profile);
+>>>>>>> migrated user foreign key in users table to profiles table
