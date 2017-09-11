a4af09670bdf8e0a2a354a306311115d38b396da
jsonlitz <jasondotlee@gmail.com>
2017-09-11 09:46:39 -0700

    rebase

--- client/src/components/Trip.jsx
+++ client/src/components/Trip.jsx
@@ -9,20 +9,11 @@ class Trip extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-<<<<<<< c661942d9e7017e8090bbebca4e67e53bd225fdc
       trip: {},
       confirms: {},
       currentUser: {},
       usersWithAccount: {}
     };
-=======
-
-    };
-    this.getTripData = this.getTripData.bind(this);
-    this.getConfirmation = this.getConfirmation.bind(this);
-    this.getUserInformation = this.getUserInformation.bind(this);
-    console.log('test');
->>>>>>> add state to trip.jsx
   }
 
   componentWillMount() {
@@ -32,7 +23,6 @@ class Trip extends React.Component {
           trip: trip.data.trip,
         });
       })
-<<<<<<< c661942d9e7017e8090bbebca4e67e53bd225fdc
       .then(() => {
         axios.get('/confirmed/byTrip')
           .then((confirms)=>{
@@ -56,48 +46,19 @@ class Trip extends React.Component {
                 });
               });
           });
-=======
-      .then(()=>{
-        console.log(this.state);
-      });
-  }
-
-  getConfirmation() {
-
-    axios.get('/confirmed/byTrip')
-      .then((confirms)=>{
-
-        this.setState({confirms});
->>>>>>> add state to trip.jsx
       })
       .catch((error) => {
         console.log(error);
       });
   }
-
-<<<<<<< c661942d9e7017e8090bbebca4e67e53bd225fdc
   render( ) {
     var style = {
       confirms: {
         textAlign: 'center'
       },
       calendar: {
-=======
-  getUserInformation() {
-    axios.get('/user/byUserId')
-      .then((user)=>{
-        this.setState({currentUser: user.data});
-      })
-      .then(()=>{
-        console.log(this.state);
-
-      });
-  }
->>>>>>> add state to trip.jsx
-
       },
       chatroom: {
-
       }
     };
 
@@ -105,18 +66,18 @@ class Trip extends React.Component {
       <div>
         <h1>{this.state.trip.tripname}</h1>
         <h3>Description: {this.state.description}</h3>
-        {Object.keys(this.state.currentUser).length !== 0 ? <Calendar 
+        {Object.keys(this.state.currentUser).length !== 0 ? <Calendar
           allUsers={this.state.usersWithAccount}
           currentUser={this.state.currentUser}
           trip={this.state.trip}/>
           : <div>loading...</div>
         }
-        {Object.keys(this.state.trip).length !== 0 ? <Chatroom 
+        {Object.keys(this.state.trip).length !== 0 ? <Chatroom
           tripId={this.state.trip.id}
           user={this.state.currentUser.display}
-          userId={this.state.currentUser.id}/> 
+          userId={this.state.currentUser.id}/>
           : <div>loading...</div> }
-        {Object.keys(this.state.confirms).length !== 0 ? <Confirmations 
+        {Object.keys(this.state.confirms).length !== 0 ? <Confirmations
           style={style.confirms}
           tripId={this.state.trip.id}
           user={this.state.currentUser}
