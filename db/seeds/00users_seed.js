5752a347c31261d62b8cc2d046b78e401fc58b73
tylerccarson <tyler.c.carson@gmail.com>
2017-09-12 20:38:02 -0700

    Update 00users_seed.js

--- db/seeds/00users_seed.js
+++ db/seeds/00users_seed.js
@@ -2,41 +2,6 @@ const models = require('../models');
 const db = require('../');
 var Promise = require('bluebird');
 
-// exports.seed = function (knex, Promise) {
-
-//   return models.Profile.where({ email: 'admin@domain.com' }).fetch()
-//     .then((profile) => {
-//       if (profile) {
-//         throw profile;
-//       }
-//       return models.Profile.forge({
-//         first: 'System',
-//         last: 'Admin',
-//         display: 'Administrator',
-//         email: 'admin@domain.com'
-//       }).save();
-//     })
-//     .error(err => {
-//       console.error('ERROR: failed to create profile');
-//       throw err;
-//     })
-//     .then((profile) => {
-//       return models.Auth.forge({
-//         type: 'local',
-//         password: 'admin123',
-//         profile_id: profile.get('id')
-//       }).save();
-//     })
-//     .error(err => {
-//       console.error('ERROR: failed to create auth');
-//       throw err;
-//     })
-//     .catch(() => {
-//       console.log('WARNING: defualt user already exists.');
-//     });
-
-// };
-
 exports.seed = function (knex, Promise) {
 
   return models.Profile.fetchAll()
