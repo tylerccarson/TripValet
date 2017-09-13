af91172e08debc76a56650055a73fc9546203175
Jason Lee <jasondotlee@gmail.com>
2017-09-12 20:50:37 -0700

    Update DashBoard.jsx

--- client/src/components/DashBoard.jsx
+++ client/src/components/DashBoard.jsx
@@ -42,7 +42,6 @@ class DashBoard extends React.Component {
         this.setState({
           trips: trips.data
         });
-        console.log("line 45 trips.data", trips.data);
       })
       .catch((error) => {
         console.log(error);
@@ -57,7 +56,6 @@ class DashBoard extends React.Component {
 
 
   render() {
-    console.log(this.state);
     return (
       <div>
         <Modal
