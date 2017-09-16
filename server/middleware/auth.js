c53a23635f73800dfb89e09f14c57e99c916572e
tyler carson <tyler.c.carson@gmail.com>
2017-09-16 15:35:00 -0700

    docker stuff

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,11 +2,13 @@ const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 var redisClient;
 
-//connect with Heroku Redis if in production
+//connect with Heroku Redis if in production -- CAN PROBABLY REMOVE THIS IF RUNNING ONE IN A CONCURRENT DOCKER CONTAINER
 if (process.env.NODE_ENV === 'production') {
+  console.log('REDIS IN PRODUCTION');
   redisClient = require('redis').createClient(process.env.REDIS_URL);
 } else {
-  redisClient = require('redis').createClient();
+  console.log('REDIS IN DEVELOPMENT');
+  redisClient = require('redis').createClient('6379', 'redis');
 }
 
 module.exports.verify = (req, res, next) => {
@@ -19,7 +21,7 @@ module.exports.verify = (req, res, next) => {
 module.exports.session = session({
   store: new RedisStore({
     client: redisClient,
-    host: 'localhost',
+    host: '172.17.0.2/16',
     port: 6379
   }),
   secret: 'more laughter, more love, more life',
