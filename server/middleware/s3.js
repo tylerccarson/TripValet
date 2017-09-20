ac480e468788b7f751e7186c2eb30fce608ce815
jsonlitz <jasondotlee@gmail.com>
2017-09-19 23:48:53 -0700

    removed secret keys from s3 middleware

--- server/middleware/s3.js
+++ server/middleware/s3.js
@@ -1,28 +1,11 @@
 const AWS = require('aws-sdk');
 const config = require('config');
-// var AWSconfig = config.get('AWS');
+
+var AWSconfig = config.get('AWS');
+
 AWS.config.update({
-  accessKeyId: 'AKIAJIQTIM2S4JSNCR4Q',
-  secretAccessKey: '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'
+  accessKeyId: AWSconfig.s3.accessKeyId,
+  secretAccessKey: AWSconfig.s3.secretAccessKey
 });
 
 module.exports.s3 = new AWS.S3();
-
-// var AWSconfig = {
-//   "s3": {
-//     "accessKeyId": "AKIAJIQTIM2S4JSNCR4Q",
-//     "secretAccessKey": "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"
-//   }
-// }
-var AWSconfig = {
-  "s3": {
-    "accessKeyId": "AKIAJIQTIM2S4JSNCR4Q",
-    "secretAccessKey": "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"
-  }
-};
-// };
-
-// AWS.config.update({
-//   "accessKeyId": AWSconfig.s3.accessKeyId,
-//   "secretAccessKey": AWSconfig.s3.secretAccessKey
-// });
